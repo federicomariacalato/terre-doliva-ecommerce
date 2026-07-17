3dd5bfa8895da0b1c48c252b242c0a531e6c7d33
Federico Maria Calato <calatofederico48@gmail.com>
2026-07-17 16:35:05 +0200

    feat: add cart and wishlist slices using types and configure query provider

---
+++ src/store/slices/wishlistSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { Product } from "../../types/store.tipes";
+
+type WishlistState = {
+  items: Product[];
+};
+
+const initialState: WishlistState = {
+  items: [],
+};
+
+const wishlistSlice = createSlice({
+  name: "wishlist",
+  initialState,
+  reducers: {
+    toogleWishlist: (state, action: PayloadAction<Product>) => {
+      const index = state.items.findIndex((i) => i.id === action.payload.id);
+      if (index >= 0) {
+        state.items.splice(index, 1);
+      } else {
+        state.items.push(action.payload);
+      }
+    },
+  },
+});
+
+export const { toogleWishlist } = wishlistSlice.actions;
+export default wishlistSlice.reducer;
